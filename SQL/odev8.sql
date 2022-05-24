4d61b5b871fc8e0eff0a5f2412dd1ed74a83ec49
Serkan Cıvak <67489733+Goopzy@users.noreply.github.com>
2022-05-24 17:11:01 +0300

    Create odev8.sql

---
+++ SQL/odev8.sql
@@ -0,0 +1,69 @@
+CREATE TABLE employee (
+id(INT), 
+name VARCHAR(50),
+birthday DATE,
+email VARCHAR(100)
+);
+
+insert into employee (id, first_name, last_name, email) values (1, 'Clywd', 'Tolliday', 'ctolliday0@irs.gov');
+insert into employee (id, first_name, last_name, email) values (2, 'Viviyan', 'Woodus', 'vwoodus1@omniture.com');
+insert into employee (id, first_name, last_name, email) values (3, 'Belvia', 'Moseby', 'bmoseby2@biblegateway.com');
+insert into employee (id, first_name, last_name, email) values (4, 'Trula', 'Prebble', 'tprebble3@fastcompany.com');
+insert into employee (id, first_name, last_name, email) values (5, 'Nicolas', 'Janes', 'njanes4@google.com.br');
+insert into employee (id, first_name, last_name, email) values (6, 'Lesley', 'Sawell', 'lsawell5@usatoday.com');
+insert into employee (id, first_name, last_name, email) values (7, 'Lanette', 'Kampshell', 'lkampshell6@independent.co.uk');
+insert into employee (id, first_name, last_name, email) values (8, 'Billy', 'Sarath', 'bsarath7@drupal.org');
+insert into employee (id, first_name, last_name, email) values (9, 'Keely', 'Ring', 'kring8@fotki.com');
+insert into employee (id, first_name, last_name, email) values (10, 'Sallie', 'Kaveney', 'skaveney9@ebay.com');
+insert into employee (id, first_name, last_name, email) values (11, 'Helen', 'Buglass', 'hbuglassa@prlog.org');
+insert into employee (id, first_name, last_name, email) values (12, 'Patricio', 'Jarvie', 'pjarvieb@fda.gov');
+insert into employee (id, first_name, last_name, email) values (13, 'Orton', 'Tale', 'otalec@businessweek.com');
+insert into employee (id, first_name, last_name, email) values (14, 'Prissie', 'Sinson', 'psinsond@g.co');
+insert into employee (id, first_name, last_name, email) values (15, 'Neda', 'Camilio', 'ncamilioe@amazon.co.uk');
+insert into employee (id, first_name, last_name, email) values (16, 'Wylma', 'Matthews', 'wmatthewsf@reuters.com');
+insert into employee (id, first_name, last_name, email) values (17, 'Daisi', 'Robberecht', 'drobberechtg@tinyurl.com');
+insert into employee (id, first_name, last_name, email) values (18, 'Felecia', 'Billson', 'fbillsonh@sourceforge.net');
+insert into employee (id, first_name, last_name, email) values (19, 'Charleen', 'Waller-Bridge', 'cwallerbridgei@tripadvisor.com');
+insert into employee (id, first_name, last_name, email) values (20, 'Damien', 'Skitterel', 'dskitterelj@yahoo.com');
+insert into employee (id, first_name, last_name, email) values (21, 'Saraann', 'Frunks', 'sfrunksk@cnet.com');
+insert into employee (id, first_name, last_name, email) values (22, 'Mamie', 'Martensen', 'mmartensenl@parallels.com');
+insert into employee (id, first_name, last_name, email) values (23, 'Portia', 'Sperrett', 'psperrettm@delicious.com');
+insert into employee (id, first_name, last_name, email) values (24, 'Carlye', 'Mathissen', 'cmathissenn@storify.com');
+insert into employee (id, first_name, last_name, email) values (25, 'Derril', 'Crosetto', 'dcrosettoo@about.com');
+insert into employee (id, first_name, last_name, email) values (26, 'Farand', 'Boyet', 'fboyetp@tripod.com');
+insert into employee (id, first_name, last_name, email) values (27, 'Yul', 'Fuzzens', 'yfuzzensq@cornell.edu');
+insert into employee (id, first_name, last_name, email) values (28, 'Flss', 'Claffey', 'fclaffeyr@utexas.edu');
+insert into employee (id, first_name, last_name, email) values (29, 'Burt', 'Jerson', 'bjersons@oakley.com');
+insert into employee (id, first_name, last_name, email) values (30, 'Cooper', 'Satch', 'csatcht@si.edu');
+insert into employee (id, first_name, last_name, email) values (31, 'Langston', 'Dahl', 'ldahlu@sciencedirect.com');
+insert into employee (id, first_name, last_name, email) values (32, 'Desi', 'Deane', 'ddeanev@creativecommons.org');
+insert into employee (id, first_name, last_name, email) values (33, 'Coretta', 'Kehir', 'ckehirw@telegraph.co.uk');
+insert into employee (id, first_name, last_name, email) values (34, 'Merilyn', 'Blandamore', 'mblandamorex@acquirethisname.com');
+insert into employee (id, first_name, last_name, email) values (35, 'Edan', 'Binny', 'ebinnyy@unicef.org');
+insert into employee (id, first_name, last_name, email) values (36, 'Montague', 'Goslin', 'mgoslinz@marriott.com');
+insert into employee (id, first_name, last_name, email) values (37, 'Joni', 'Johnsson', 'jjohnsson10@dailymail.co.uk');
+insert into employee (id, first_name, last_name, email) values (38, 'Graeme', 'Gunning', 'ggunning11@youku.com');
+insert into employee (id, first_name, last_name, email) values (39, 'Kaia', 'Smedmoor', 'ksmedmoor12@mac.com');
+insert into employee (id, first_name, last_name, email) values (40, 'Merle', 'Castanos', 'mcastanos13@illinois.edu');
+insert into employee (id, first_name, last_name, email) values (41, 'Tamma', 'Kliesl', 'tkliesl14@earthlink.net');
+insert into employee (id, first_name, last_name, email) values (42, 'Lyssa', 'Turfus', 'lturfus15@theatlantic.com');
+insert into employee (id, first_name, last_name, email) values (43, 'Sterling', 'Sendall', 'ssendall16@wikipedia.org');
+insert into employee (id, first_name, last_name, email) values (44, 'Deidre', 'Issit', 'dissit17@imageshack.us');
+insert into employee (id, first_name, last_name, email) values (45, 'Alleen', 'Caveau', 'acaveau18@vimeo.com');
+insert into employee (id, first_name, last_name, email) values (46, 'Celie', 'Willshee', 'cwillshee19@boston.com');
+insert into employee (id, first_name, last_name, email) values (47, 'Arleen', 'Rosenblath', 'arosenblath1a@wikipedia.org');
+insert into employee (id, first_name, last_name, email) values (48, 'Maxwell', 'McGowran', 'mmcgowran1b@eventbrite.com');
+insert into employee (id, first_name, last_name, email) values (49, 'Randy', 'Luck', 'rluck1c@howstuffworks.com');
+insert into employee (id, first_name, last_name, email) values (50, 'Ardenia', 'Sorrell', 'asorrell1d@uol.com.br');
+
+
+Update FROM Employee
+  SET first_name = Atakan,
+      last_name=  Onat,
+      email=atakan.onat@gmail.com,
+ WHERE first_name= 'A%';
+ 
+ 
+ DELETE FROM Employee
+  where first_name = 'A%';
+    
