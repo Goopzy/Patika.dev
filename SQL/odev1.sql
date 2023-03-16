c1dd81b480623757173843107e839ec6181d9ee6
Serkan Cıvak <67489733+Goopzy@users.noreply.github.com>
2023-03-16 23:14:33 +0300

    Update odev1.sql

--- SQL/odev1.sql
+++ SQL/odev1.sql
@@ -1,6 +1,6 @@
 Select * From film;
 
-Select * From film Where lenght > 60 AND lenght < 75;
+Select * From film Where lenght > 60 AND lenght < 75 ;
 
 Select * From film Where rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
 
